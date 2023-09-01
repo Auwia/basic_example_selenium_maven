4c1123c1a6ba115ff10b56918daac05cd542b1cc
Auwia <massimo.manganiello@gmail.com>
2023-09-01 21:51:40 +0200

    take a printscreen

--- src/main/java/ch/max/App.java
+++ src/main/java/ch/max/App.java
@@ -1,9 +1,14 @@
 package ch.max;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.OutputType;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
 
 public class App {
     public static void main(String[] args) {
@@ -14,6 +19,16 @@ public class App {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://www.google.com");
+
+        // Handle the cookies popup (if present)
+        try {
+            WebElement agreeButton = driver.findElement(By.id("L2AGLb"));
+            if (agreeButton.isDisplayed()) {
+                agreeButton.click();
+            }
+        } catch (Exception e) {
+            // Cookies popup not found or not visible, continue with the script
+        }
 
         WebElement searchBox = driver.findElement(By.name("q"));
         searchBox.sendKeys("testing tool online org");
@@ -26,9 +41,6 @@ public class App {
             e.printStackTrace();
         }
 
-        WebElement firstResult = driver.findElement(By.cssSelector("div.r > a"));
-        firstResult.click();
-
         // Wait for page to load
         try {
             Thread.sleep(5000);
@@ -36,7 +48,22 @@ public class App {
             e.printStackTrace();
         }
 
-        // Perform any further actions on the opened page here
+        // Capture a screenshot of a specific element (example: search box)
+        WebElement elementToCapture = driver.findElement(By.name("q"));
+        File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(screenshotFile, new File("element_screenshot.png"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Capture a screenshot of the full page
+        File fullScreenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(fullScreenshotFile, new File("fullpage_screenshot.png"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         driver.quit();
     }
